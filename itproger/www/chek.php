61b8b6f2cadd8c7af6c7b8fc4ab448b727d88a92
sumakaki <sumakaki37rus@gmail.com>
2019-05-13 16:00:32 +0300

    Made somo shanges

--- itproger/www/chek.php
+++ itproger/www/chek.php
@@ -5,24 +5,27 @@
 		$name = $_POST['name'];
 		$password = $_POST['password'];
 
-		$sql_1 = 'SELECT id FROM user WHERE login = :login && password = :password';
-	    $query = $pdo->prepare($sql_1);
-	    $query->execute(['login'=>$login, 'password'=>$password]);
-	    $users = $query->fetch(PDO::FETCH_OBJ);
-	    
+		$hash = "sdfjsdkhgs234jh324SDk";
+		$pass = md5($password . $hash);
+
+		$sql_1 = 'SELECT id FROM user WHERE login = :login && password = :pass';
+			$query = $pdo->prepare($sql_1);
+			$query->execute(['login'=>$login, 'password'=>$pass]);
+			$users = $query->fetch(PDO::FETCH_OBJ);
+
 	    if(!empty($users->id)){
 			exit('Такой пользователь существует');
 		}
-		
+
 		$sql_2 = 'INSERT INTO user(login, name, password) VALUES(:login, :name, :password)';
 		$query = $pdo->prepare($sql_2);
 		$query->execute(['login'=>$login, 'name'=>$name, 'password'=>$password]);
-	
+
 		if(empty(!$sql_2) && $sql_2 !==$sql_2->id ){
 			echo 'Вы успешно зарегестрированы';
 		}else{
 			echo 'Вы не зарегестрированы';
 		}
 
-	
+
 ?>
